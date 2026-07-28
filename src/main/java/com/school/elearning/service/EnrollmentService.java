08d99aec2ae0886403908ef81a39235836f1176c
mohamed <m.eddinari5197@uca.ac.ma>
2026-07-28 12:43:38 +0200

    feat :add ExceptionHandling to the busniss Logic

--- src/main/java/com/school/elearning/service/EnrollmentService.java
+++ src/main/java/com/school/elearning/service/EnrollmentService.java
@@ -4,16 +4,19 @@ import com.school.elearning.dto.request.EnrollmentRequest;
 import com.school.elearning.dto.response.EnrollmentResponse;
 import com.school.elearning.entity.Enrollment;
 import com.school.elearning.entity.User;
+import com.school.elearning.exception.RessourceNotFoundException;
 import com.school.elearning.mapper.EnrollmentMapper;
 import com.school.elearning.repository.EnrollmentRepository;
 import com.school.elearning.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class EnrollmentService {
     private final EnrollmentRepository enrollmentRepository;
     private final EnrollmentMapper enrollmentMapper;
@@ -23,7 +26,7 @@ public class EnrollmentService {
 
     public EnrollmentResponse createEnrollment(EnrollmentRequest request) {
         User user = userRepository.findById(request.userId())
-                .orElseThrow(() -> new RuntimeException("User not found with id :" + request.userId()));
+                .orElseThrow(() -> new RessourceNotFoundException("User not found with id :" + request.userId()));
         Enrollment enrollment = enrollmentMapper.toEnrollment(request);
         enrollment.setUser(user);
         enrollmentRepository.save(enrollment);
@@ -33,20 +36,20 @@ public class EnrollmentService {
 
     public void deleteEnrollment(Long id) {
         Enrollment enrollment = enrollmentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Enrollment not found with id :" + id));
+                .orElseThrow(() -> new RessourceNotFoundException("Enrollment not found with id :" + id));
         enrollmentRepository.delete(enrollment);
     }
 
     public EnrollmentResponse updateEnrollment(Long id, EnrollmentRequest request) {
         Enrollment enrollment = enrollmentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Enrollment not found with id :" + id));
+                .orElseThrow(() -> new RessourceNotFoundException("Enrollment not found with id :" + id));
         enrollmentMapper.updateEnrollment(request ,enrollment);
         enrollmentRepository.save(enrollment);
         return enrollmentMapper.toEnrollmentResponse(enrollment);
     }
     public EnrollmentResponse findEnrollmentById(Long id) {
         Enrollment enrollment = enrollmentRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Enrollment not found with id :" + id));
+                .orElseThrow(() -> new RessourceNotFoundException("Enrollment not found with id :" + id));
         return enrollmentMapper.toEnrollmentResponse(enrollment);
     }
 
